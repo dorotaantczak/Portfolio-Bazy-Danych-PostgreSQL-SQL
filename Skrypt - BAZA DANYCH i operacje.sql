19e072ee347d9bb465651f22b4f25bebeb7aaa33
dorotaantczak <62247514+dorotaantczak@users.noreply.github.com>
2023-09-07 16:23:56 +0200

    Update Skrypt - BAZA DANYCH i operacje.sql

--- Skrypt - BAZA DANYCH i operacje.sql
+++ Skrypt - BAZA DANYCH i operacje.sql
@@ -11,185 +11,179 @@ DROP TABLE IF EXISTS producent;
 -- Tworzenie tabeli klient
 
 CREATE TABLE klient (
-  klient_id              INTEGER,
+  klient_id              SERIAL PRIMARY KEY,
   imie                   VARCHAR(50),
   nazwisko               VARCHAR(50),
   data_urodzenia         DATE,
-  numer_identyfikacyjny  INTEGER,
-  CONSTRAINT klient_pk PRIMARY KEY (klient_id)
+  numer_identyfikacyjny  INTEGER
 );
 
 -- Wstawianie danych do tabeli klient
 
-INSERT INTO klient (klient_id, imie, nazwisko, data_urodzenia, numer_identyfikacyjny) 
+INSERT INTO klient (imie, nazwisko, data_urodzenia, numer_identyfikacyjny) 
 VALUES 
-(1, 'Jan', 'Kowalski', '1990-01-15', 12345),
-(2, 'Anna', 'Nowak', '1985-06-20', 67890),
-(3, 'Marcin', 'Wiśniewski', '1995-03-10', 54321),
-(4, 'Magda', 'Lewandowska', '1992-11-30', 98765),
-(5, 'Piotr', 'Dąbrowski', '1988-09-25', 24680),
-(6, 'Karolina', 'Zielińska', '1993-08-12', 13579),
-(7, 'Michał', 'Król', '1987-04-05', 97531),
-(8, 'Alicja', 'Jankowska', '1991-02-18', 86420),
-(9, 'Krzysztof', 'Kaczmarek', '1998-07-22', 74185),
-(10, 'Ewa', 'Wójcik', '1984-12-03', 36987);
+('Jan', 'Kowalski', '1990-01-15', 12345),
+('Anna', 'Nowak', '1985-06-20', 67890),
+('Marcin', 'Wiśniewski', '1995-03-10', 54321),
+('Magda', 'Lewandowska', '1992-11-30', 98765),
+('Piotr', 'Dąbrowski', '1988-09-25', 24680),
+('Karolina', 'Zielińska', '1993-08-12', 13579),
+('Michał', 'Król', '1987-04-05', 97531),
+('Alicja', 'Jankowska', '1991-02-18', 86420),
+('Krzysztof', 'Kaczmarek', '1998-07-22', 74185),
+('Ewa', 'Wójcik', '1984-12-03', 36987);
 
 -- Tworzenie tabeli klient_typ_konta
 
 CREATE TABLE klient_typ_konta (
-  klient_typ_konta_id   INTEGER,
+  klient_typ_konta_id   SERIAL PRIMARY KEY,
   klient_id             INTEGER,
   typ_konta             VARCHAR(50),
-  profil_klienta        VARCHAR(50),
-  CONSTRAINT klient_typ_konta_pk PRIMARY KEY (klient_typ_konta_id)
+  profil_klienta        VARCHAR(50)
 );
 
 -- Wstawianie danych do tabeli klient_typ_konta
 
-INSERT INTO klient_typ_konta (klient_typ_konta_id, klient_id, typ_konta, profil_klienta)
+INSERT INTO klient_typ_konta (klient_id, typ_konta, profil_klienta)
 VALUES
-  (1, 1, 'Premium', 'Indywidualny'),
-  (2, 2, 'Gold', 'Firmowy'),
-  (3, 3, 'Standard', 'Indywidualny'),
-  (4, 4, 'Standard', 'Firmowy'),
-  (5, 5, 'Standard', 'Indywidualny'),
-  (6, 6, 'Gold', 'Firmowy'),
-  (7, 7, 'Gold', 'Indywidualny'),
-  (8, 8, 'Premium', 'Indywidualny'),
-  (9, 9, 'Premium', 'Firmowy'),
-  (10, 10, 'Premium', 'Firmowy');
+  (1, 'Premium', 'Indywidualny'),
+  (2, 'Gold', 'Firmowy'),
+  (3, 'Standard', 'Indywidualny'),
+  (4, 'Standard', 'Firmowy'),
+  (5, 'Standard', 'Indywidualny'),
+  (6, 'Gold', 'Firmowy'),
+  (7, 'Gold', 'Indywidualny'),
+  (8, 'Premium', 'Indywidualny'),
+  (9, 'Premium', 'Firmowy'),
+  (10, 'Premium', 'Firmowy');
 
 -- Tworzenie tabeli adres_klienta
 
 CREATE TABLE adres_klienta (
-  adres_id      INTEGER,
+  adres_id      SERIAL PRIMARY KEY,
   klient_id     INTEGER,
   miasto        VARCHAR(50),
   kraj          VARCHAR(50),
-  ulica         VARCHAR(50),
-  CONSTRAINT adres_klienta_pk PRIMARY KEY (adres_id)
+  ulica         VARCHAR(50)
 );
 
 -- Wstawianie danych do tabeli adres_klienta
 
-INSERT INTO adres_klienta (adres_id, klient_id, miasto, kraj, ulica)
+INSERT INTO adres_klienta (klient_id, miasto, kraj, ulica)
 VALUES
-  (1, 1, 'Warsaw', 'Poland', 'Śliczna 54/3'),
-  (2, 2, 'Krakow', 'Poland', 'Szeroka 134'),
-  (3, 3, 'Radom', 'Poland', 'Lipowa 78/4'),
-  (4, 4, 'Kielce', 'Poland', 'Wiosenna 83/3'),
-  (5, 5, 'Londyn', 'UK', 'Main Street 11'),
-  (6, 6, 'Glasgow', 'UK', 'Narrow Street 78'),
-  (7, 7, 'Leeds', 'UK', 'Pretty Street 467'),
-  (8, 8, 'Berlin', 'Germany', 'Wide Street 564/3'),
-  (9, 9, 'Monachium', 'Germany', 'Green Street 12'),
-  (10, 10, 'Oslo', 'Norwegia', 'Water Street 34');
+  (1, 'Warsaw', 'Poland', 'Śliczna 54/3'),
+  (2, 'Krakow', 'Poland', 'Szeroka 134'),
+  (3, 'Radom', 'Poland', 'Lipowa 78/4'),
+  (4, 'Kielce', 'Poland', 'Wiosenna 83/3'),
+  (5, 'Londyn', 'UK', 'Main Street 11'),
+  (6, 'Glasgow', 'UK', 'Narrow Street 78'),
+  (7, 'Leeds', 'UK', 'Pretty Street 467'),
+  (8, 'Berlin', 'Germany', 'Wide Street 564/3'),
+  (9, 'Monachium', 'Germany', 'Green Street 12'),
+  (10, 'Oslo', 'Norwegia', 'Water Street 34');
 
 -- Tworzenie tabeli zamowienie
 
 CREATE TABLE zamowienie (
-  zamowienie_id     INTEGER,
+  zamowienie_id     SERIAL PRIMARY KEY,
   klient_id         INTEGER,
   data_zamowienia   DATE,
-  status_zamowienia VARCHAR(50),
-  CONSTRAINT zamowienie_pk PRIMARY KEY (zamowienie_id)
+  status_zamowienia VARCHAR(50)
 );
 
 -- Wstawianie danych do tabeli zamowienie
 
-INSERT INTO zamowienie (zamowienie_id, klient_id, data_zamowienia, status_zamowienia)
+INSERT INTO zamowienie (klient_id, data_zamowienia, status_zamowienia)
 VALUES
-  (1, 1, '2023-08-01', 'Nowe'),
-  (2, 2, '2023-08-05', 'W trakcie'),
-  (3, 3, '2023-07-25', 'Wysłane'),
-  (4, 4, '2023-08-10', 'Nowe'),
-  (5, 5, '2023-08-02', 'Wysłane'),
-  (6, 6, '2023-07-28', 'Zakończone'),
-  (7, 7, '2023-08-04', 'W trakcie'),
-  (8, 8, '2023-08-08', 'Nowe'),
-  (9, 9, '2023-07-30', 'Zakończone'),
-  (10, 10, '2023-08-03', 'Wysłane');
+  (1, '2023-08-01', 'Nowe'),
+  (2, '2023-08-05', 'W trakcie'),
+  (3, '2023-07-25', 'Wysłane'),
+  (4, '2023-08-10', 'Nowe'),
+  (5, '2023-08-02', 'Wysłane'),
+  (6, '2023-07-28', 'Zakończone'),
+  (7, '2023-08-04', 'W trakcie'),
+  (8, '2023-08-08', 'Nowe'),
+  (9, '2023-07-30', 'Zakończone'),
+  (10, '2023-08-03', 'Wysłane');
 
 -- Tworzenie tabeli producent
 
 CREATE TABLE producent (
-  producent_id      INTEGER,
+  producent_id      SERIAL PRIMARY KEY,
   nazwa             VARCHAR(50),
-  umowa_wspolpraca  BOOLEAN,
-  CONSTRAINT producent_pk PRIMARY KEY (producent_id)
+  umowa_wspolpraca  BOOLEAN
 );
 
 -- Wstawianie danych do tabeli producent
 
-INSERT INTO producent (producent_id, nazwa, umowa_wspolpraca)
+INSERT INTO producent (nazwa, umowa_wspolpraca)
 VALUES
-  (1, 'Firma A', TRUE),
-  (2, 'Firma B', FALSE),
-  (3, 'Firma C', TRUE),
-  (4, 'Firma D', TRUE),
-  (5, 'Firma E', FALSE),
-  (6, 'Firma F', TRUE),
-  (7, 'Firma G', FALSE),
-  (8, 'Firma H', TRUE),
-  (9, 'Firma I', FALSE),
-  (10, 'Firma J', TRUE);
+  ('Firma A', TRUE),
+  ('Firma B', FALSE),
+  ('Firma C', TRUE),
+  ('Firma D', TRUE),
+  ('Firma E', FALSE),
+  ('Firma F', TRUE),
+  ('Firma G', FALSE),
+  ('Firma H', TRUE),
+  ('Firma I', FALSE),
+  ('Firma J', TRUE);
 
 -- Tworzenie tabeli Produkt
 
 CREATE TABLE produkt (
-  produkt_id            INTEGER,
+  produkt_id            SERIAL PRIMARY KEY,
   cena                  NUMERIC(10, 2),
   nazwa                 VARCHAR(50),
   dostepna_ilosc_magazyn INTEGER,
   kategoria             VARCHAR(50),
-  producent_id       INTEGER,
-  CONSTRAINT produkt_pk PRIMARY KEY (produkt_id),
-  CONSTRAINT producent_id_fk FOREIGN KEY (producent_id) REFERENCES producent (producent_id));
+  producent_id          INTEGER,
+  CONSTRAINT producent_id_fk FOREIGN KEY (producent_id) REFERENCES producent (producent_id)
+);
 
 -- Wstawianie danych do tabeli produkt
 
-INSERT INTO produkt (produkt_id, cena, nazwa, dostepna_ilosc_magazyn, kategoria)
+INSERT INTO produkt (cena, nazwa, dostepna_ilosc_magazyn, kategoria)
 VALUES
-  (1, 25.99, 'Laptop', 10, 'Elektronika'),
-  (2, 9.99, 'Mysz komputerowa', 50, 'Akcesoria komputerowe'),
-  (3, 199.00, 'Smartfon', 20, 'Telefony'),
-  (4, 49.99, 'Słuchawki', 30, 'Akcesoria audio'),
-  (5, 399.00, 'Telewizor', 5, 'Elektronika'),
-  (6, 29.50, 'Klawiatura', 25, 'Akcesoria komputerowe'),
-  (7, 59.99, 'Aparat fotograficzny', 15, 'Fotografia'),
-  (8, 12.75, 'Powerbank', 40, 'Akcesoria mobilne'),
-  (9, 149.99, 'Konsola do gier', 8, 'Gry'),
-  (10, 7.25, 'Podkładka pod mysz', 100, 'Akcesoria komputerowe');
+  (25.99, 'Laptop', 10, 'Elektronika'),
+  (9.99, 'Mysz komputerowa', 50, 'Akcesoria komputerowe'),
+  (199.00, 'Smartfon', 20, 'Telefony'),
+  (49.99, 'Słuchawki', 30, 'Akcesoria audio'),
+  (399.00, 'Telewizor', 5, 'Elektronika'),
+  (29.50, 'Klawiatura', 25, 'Akcesoria komputerowe'),
+  (59.99, 'Aparat fotograficzny', 15, 'Fotografia'),
+  (12.75, 'Powerbank', 40, 'Akcesoria mobilne'),
+  (149.99, 'Konsola do gier', 8, 'Gry'),
+  (7.25, 'Podkładka pod mysz', 100, 'Akcesoria komputerowe');
 
 -- Tworzenie tabeli zamowienie_produktu
 
 CREATE TABLE zamowienie_produktu (
-  zamowienie_produktu_id   INTEGER,
+  zamowienie_produktu_id   SERIAL PRIMARY KEY,
   produkt_id               INTEGER,
   zamowienie_id            INTEGER,
   ilosc_zamowiona          INTEGER,
   cena_za_sztuke           NUMERIC(10, 2),
   dokument_zakupu          VARCHAR(50),
 
-  CONSTRAINT zamowienie_produktu_pk PRIMARY KEY (zamowienie_produktu_id),
   CONSTRAINT produkt_id_fk FOREIGN KEY (produkt_id) REFERENCES produkt (produkt_id),
   CONSTRAINT zamowienie_id_fk FOREIGN KEY (zamowienie_id) REFERENCES zamowienie (zamowienie_id)
 );
 
 -- Wstawianie danych do tabeli zamowienie_produktu
 
-INSERT INTO zamowienie_produktu (zamowienie_produktu_id, produkt_id, zamowienie_id, ilosc_zamowiona, cena_za_sztuke, dokument_zakupu)
+INSERT INTO zamowienie_produktu (produkt_id, zamowienie_id, ilosc_zamowiona, cena_za_sztuke, dokument_zakupu)
 VALUES
-  (1, 1, 1, 5, 10.99, 'ZK123'),
-  (2, 2, 1, 2, 25.50, 'ZK124'),
-  (3, 3, 2, 10, 5.75, 'ZK125'),
-  (4, 4, 3, 3, 8.20, 'ZK126'),
-  (5, 5, 3, 7, 15.00, 'ZK127'),
-  (6, 6, 4, 1, 100.00, 'ZK128'),
-  (7, 7, 5, 4, 12.75, 'ZK129'),
-  (8, 8, 6, 6, 50.25, 'ZK130'),
-  (9, 9, 7, 8, 18.99, 'ZK131'),
-  (10, 10, 8, 2, 30.00, 'ZK132');
+  (1, 1, 5, 10.99, 'ZK123'),
+  (2, 1, 2, 25.50, 'ZK124'),
+  (3, 2, 10, 5.75, 'ZK125'),
+  (4, 3, 3, 8.20, 'ZK126'),
+  (5, 3, 7, 15.00, 'ZK127'),
+  (6, 4, 1, 100.00, 'ZK128'),
+  (7, 5, 4, 12.75, 'ZK129'),
+  (8, 6, 6, 50.25, 'ZK130'),
+  (9, 7, 8, 18.99, 'ZK131'),
+  (10, 8, 2, 30.00, 'ZK132');
 
 -- Operacje na tabelach
 
